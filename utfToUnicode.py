1bc7f69c85d032e26779ed3a02290bc339bd63f5
Jacob110 <yjj.jacob@gmail.com>
2014-01-26 21:50:23 +0800

    add some comments

--- utfToUnicode.py
+++ utfToUnicode.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
 import MySQLdb
 host='localhost'
-password='yjj'
-user='root'
-dbname='yjj'
+password='yourpwd'
+user='youruser'
+dbname='yourdbname'
 
 db=MySQLdb.connect(host=host,user=user,passwd=password,db=dbname)
 cursor=db.cursor()
